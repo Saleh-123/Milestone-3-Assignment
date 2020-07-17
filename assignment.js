1675797a9c2eb2be91987a17833fb5ab7f332d0b
Saleh-123 <67531889+Saleh-123@users.noreply.github.com>
2020-07-17 22:36:07 +0600

    Wood done

--- assignment.js
+++ assignment.js
@@ -5,5 +5,11 @@ function feetToMile(feet){
 
 
 function woodCalculator(chair,table,bed){
+    var chairCount = chair *1;
+    var tableCount = table *3;
+    var bedCount = bed * 5;
     
+    var totalWood = chairCount + tableCount + bedCount;
+    return totalWood;
 }
+
